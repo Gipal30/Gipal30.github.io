ab275d333a1087051723b62f34aa8bdf13fe2a8a
Gipal30 <legitorlacs@gmail.com>
2025-02-28 03:27:52 +0800

    Changed Title

--- TSA 3/script.js
+++ TSA 3/script.js
@@ -1,20 +1,16 @@
-// Function to Append Values to Display
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
-// Function to Clear Display
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
-// Function to Delete the Last Character (Backspace)
 function deleteLast() {
     let display = document.getElementById('display');
     display.value = display.value.slice(0, -1);
 }
 
-// Function to Calculate Result
 function calculateResult() {
     try {
         document.getElementById('display').value = eval(document.getElementById('display').value);
